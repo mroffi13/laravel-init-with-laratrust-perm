bc984b5849fe38a4bb7bb49a63d4c0d6cb4508cf
Mohammad Roffi <aeropie13@gmail.com>
2022-05-12 17:10:30 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,4 @@
+<div class="float-right d-none d-sm-block">
+    <b>Version</b> {{config('app.version')}}
+</div>
+<strong>Copyright © {{date('Y')}} <a href="http://rlangcodes.my.id">Roffi Apps</a>.</strong> All rights reserved.
